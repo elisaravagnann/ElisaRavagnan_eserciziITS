c4cf3bdcebb8663c917f05803fa30389ae736e40
elisaravagnann <95124921+elisaravagnann@users.noreply.github.com>
2023-03-29 15:00:00 +0200

    Update routes.py

--- ERavagnan_ProgettoFlask/ristorante/routes.py
+++ ERavagnan_ProgettoFlask/ristorante/routes.py
@@ -9,20 +9,6 @@ import hashlib
 from datetime import datetime
 
 
-
-
-'''posts = [
-    {"author": "Pico de Paperis",
-     "title": "Thi is a Blog",
-     "content": "This is a post by PdP. Se il post e' lunghissimo non cambia niente ?",
-     "date_posted": "16-11-2022 15:48"
-     },
-    {"author": "Paolino Paperino",
-     "title": "Qua Qua Qua",
-     "content": "Paperino ha scritto in italiano",
-     "date_posted": "15-11-2022 20:48"
-     }
-]'''
 #App di home
 @app.route("/")
 @app.route("/home")
